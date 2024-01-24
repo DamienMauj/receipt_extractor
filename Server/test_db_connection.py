1739ce09c8cfc8ba06a9b8bc38c7b2a3efa9311f
DamienMauj <damienmaujean@gmail.com>
2024-01-24 17:59:59 +0400

    feat: add docker for database structure and app

---
+++ Server/test_db_connection.py
@@ -0,0 +1,19 @@
+# test_db_connection.py
+
+import psycopg2
+from psycopg2 import OperationalError
+
+def create_connection():
+    try:
+        conn = psycopg2.connect(
+            host="db",  # This is the hostname of the db service in docker-compose
+            database="mydatabase",
+            user="user",
+            password="password"
+        )
+        print(f"Connection to PostgreSQL DB successful - {conn} ")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return conn
+
+create_connection()

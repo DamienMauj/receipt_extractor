7ba6185d7b7c97d1ae53f634b7404c9082776810
DamienMauj <damienmaujean@gmail.com>
2024-01-25 21:21:51 +0400

    feat: autocreation fo wanted table structure

--- Server/postgres_init/init_db.sql
+++ Server/postgres_init/init_db.sql
@@ -1,5 +1,23 @@
-CREATE TABLE IF NOT EXISTS my_table (
-    id SERIAL PRIMARY KEY,
-    column1 VARCHAR(255),
-    column2 INT
+CREATE TABLE IF NOT EXISTS "User" (
+    "User_id" UUID PRIMARY KEY,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "Raw_Receipt" (
+    "Receipt_id" UUID PRIMARY KEY,
+    "Export_datetime" TIMESTAMP NOT NULL,
+    "Image" BYTEA NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "Receipt" (
+    "Receipt_id" UUID PRIMARY KEY,
+    "Raw_total" VARCHAR(255),
+    "Raw_shop_information" VARCHAR(255),
+    "Raw_time" VARCHAR(255),
+    "Raw_item_purchase" VARCHAR(255),
+    "Total" DOUBLE PRECISION,
+    "Shop_information" VARCHAR(255),
+    "Time" TIMESTAMP,
+    "Item_purchase" VARCHAR(255)
 );
